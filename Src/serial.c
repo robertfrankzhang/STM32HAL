6940aba3e03db140d7bd2d27dba21094398ecfef
Your Name <you@example.com>
2019-01-13 10:08:10 -0500

    USB receive

--- Src/serial.c
+++ Src/serial.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "serial.h"
+#include "usbd_cdc_if.h"
+
+extern USBD_HandleTypeDef hUsbDeviceFS;
+
+uint8_t serial_rxbuf[SERIAL_MSGMAX];
+uint8_t serial_rxlen=0;
+
 void serial_init(void){
+  serial_rxlen= 0;
 }
 
-void serial_printf(const char *fmt,...){
+uint8_t serial_send(uint8_t *data,uint16_t len){
+  if (hUsbDeviceFS.dev_state == USBD_STATE_CONFIGURED)
+    return CDC_Transmit_FS(data,len);
+  return 0;
+}
+
+uint8_t serial_printf(const char *fmt,...){
   va_list ap;
   va_start(ap,fmt);
-  char line[32];
+  char line[64];
   vsnprintf(line,32,fmt,ap);
   va_end(ap);
-  CDC_Transmit_FS(line,strlen(line));
+  if (hUsbDeviceFS.dev_state == USBD_STATE_CONFIGURED)
+    return CDC_Transmit_FS((uint8_t*)line,strlen(line));
+  return 0;
+}
+
+void serial_rcvd(uint8_t* data, uint32_t len){
+	if(len>SERIAL_MSGMAX)
+		len = SERIAL_MSGMAX;
+	memcpy(serial_rxbuf,data,len);
+	serial_rxlen = len;
+}
+
+uint16_t getSerialData(uint8_t *buf,uint16_t buflen){
+	uint16_t len = serial_rxlen;
+	if(len > buflen)
+		len = buflen;
+	memcpy(buf,serial_rxbuf,len);
+	serial_rxlen = 0;
+	return len;
 }
--- Src/serial.h
+++ Src/serial.h
@@ -1,8 +1,20 @@
 #ifndef _serial_h
 #define _serial_h
-#include "usbd_cdc_if.h"
+
 #include <stdint.h>
 void serial_init(void);
-#define serial_send(data,len) CDC_Transmit_FS(data,len)
-void serial_printf(const char *fmt,...);
+uint8_t serial_send(uint8_t *data,uint16_t len);
+uint8_t serial_printf(const char *fmt,...);
+
+/* call from usbd_cdc_if.c - CDC_Receive_FS() */
+void serial_rcvd(uint8_t* data, uint32_t len);
+
+#define SERIAL_MSGMAX 64
+
+/* api for state_machine */
+/* uint8_t rcvddata[64] */
+/* uint16_t rcvedlen; */
+/* rcvdlen = getSerialData(rcvdbuf,64); */
+/* if( rcvdlen > 0) { dosomething }; */
+uint16_t getSerialData(uint8_t *buf,uint16_t buflen);
 #endif // _serial_h
